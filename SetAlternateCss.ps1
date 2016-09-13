f2d16ea3ff4756aba1deff3c77cc0089f812c081
wahidsaleemi <onewahid@live.com>
2016-09-13 11:18:00 -0700

    Added RecordSiteInfo function

--- SetAlternateCss.ps1
+++ SetAlternateCss.ps1
@@ -1,11 +1,72 @@
-$12hive = (join-path –path “c:\program files\common files\microsoft shared\web server extensions\12\" -childPath "\isapi")
+function SetAlternateCss {
+[CmdletBinding()]
+  <# 
+  .SYNOPSIS 
+  Sets or documents (only) the AlternateCssUrl properties of a SharePoint web (SPWeb). 
+  .DESCRIPTION 
+  Sets or documents (only) the AlternateCssUrl properties of a SharePoint web (SPWeb). Specify the -File parameter which contains a list of urls to work against.
+  .EXAMPLE 
+  SetAlternateCss -File C:\temp\mySiteList.csv
+  .EXAMPLE 
+  SetAlternateCss -File C:\temp\mySiteList.csv -DocumentExisting
+  .PARAMETER File 
+  Specify the path and name of the CSV file containing a list of URLs for the SharePoint sites.
+  .PARAMETER DocumentExisting 
+  Use this switch to only document the existing values. It will create a file at $env:TEMP\SiteInfo.csv
+  #> 
+Param(
+[Parameter(Position=0,Mandatory=$True,HelpMessage="Enter a file name to process")]
+[string]$File="C:\temp\sites.csv",
+[Parameter()]
+[switch]$DocumentExisting
+)
+
+#Set some parameters
+$siteinfo = "$env:TEMP\SiteInfo.csv"
+
+#Load the DLL's
+$12hive = (join-path -path "c:\program files\common files\microsoft shared\web server extensions\12\" -childPath "\isapi")
 Get-ChildItem -Path (join-path -path $12hive -childPath "Microsoft*.dll") | ForEach-Object {[System.Reflection.Assembly]::LoadFrom((join-path -path $12hive -childPath $_.Name))}
 
-$spsite = [Microsoft.SharePoint.SPSite]("http://moss.cravenet.com/sites/site1")
-foreach ($web in $spsite.AllWebs)
-            {
-            Write-Host "Updating $web.Title"
-            $web.AlternateCssUrl = "/Style Library/CustomStyles/custom.css"
-            $web.AllProperties["__InheritsAlternateCssUrl"] = $True
-            $web.Update()
-            }
+#Get the file
+$sites = Get-Content $file
+
+#Get the existing AlternateCssUrl
+function RecordSiteInfo {
+foreach ($site in $sites)
+    {
+        $spsite = [Microsoft.SharePoint.SPSite]($site)
+        foreach ($web in $spsite.AllWebs)
+                    {
+                    Write-Output "Retrieving information from $web.Title"
+                    $web | select Url, AlternateCssUrl, AllProperties["__InheritsAlternateCssUrl"] | Export-Csv $siteinfo
+                    }
+    }
+}
+
+
+#Define the function to change the AlternateCssUrl
+function ProcessSites {
+foreach ($site in $sites)
+    {
+        $spsite = [Microsoft.SharePoint.SPSite]($site)
+        foreach ($web in $spsite.AllWebs)
+                    {
+                    Write-Output "Updating $web.Title"
+                    $web.AlternateCssUrl = "/Style Library/CustomStyles/custom.css"
+                    $web.AllProperties["__InheritsAlternateCssUrl"] = $True
+                    $web.Update()
+                    }
+    }
+}
+
+#Execute
+If ($DocumentExisting)
+    {
+        RecordSiteInfo
+    }
+Else {ProcessSites}
+
+}
+
+SetAlternateCss
